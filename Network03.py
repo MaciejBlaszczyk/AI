20a2bad219383a87ccb933d3f1d252dbffb7abb3
Maciej Blaszczyk <maciej_blaszczyk96@wp.pl>
2017-02-13 20:03:55 +0100

    main logic of network implemented

--- Network03.py
+++ Network03.py
@@ -1,4 +1,14 @@
 from math import sqrt
+from random import random
+
+
+def normalize(signals):
+    strength = 0
+    for signal in signals:
+        strength += signal * signal
+    strength = sqrt(strength)
+    for i in range(len(signals)):
+        signals[i] /= strength
 
 
 class Neuron:
@@ -8,31 +18,43 @@ class Neuron:
     def __init__(self):
         pass
 
-    def normalize(self, signals):
-        strength = 0
-        for signal in signals:
-            strength += signal * signal
-        strength = sqrt(strength)
+    def randomize(self, min, max, inputCount):
+        length = max - min
+        for i in range(inputCount):
+            self.weights.append(min + length * random())
 
-        for signal in signals:
-            signal /= strength
+    def learn(self, signals, expectedOutput, ratio, previousResponse, previousError):
+        previousResponse[0] = self.response(signals)
+        previousError[0] = expectedOutput - previousResponse[0]
+        for i in range(len(self.weights)):
+            self.weights[i] += ratio * previousError[0] * signals[i]
+
+    def response(self, signals):
+        result = 0.
+        for signal, weight in zip(signals, self.weights):
+            result += signal * weight
+        return result
 
 
 class Element:
     inputs = []
-    expectedOutputs = []
+    expectedOutput = 0.
     comment = ""
 
-    def __init__(self, inputs, expectedOutputs, comment):
+    def __init__(self, inputs, expectedOutput, comment):
         self.inputs = inputs
-        self. expectedOutputs = expectedOutputs
+        self.expectedOutput = expectedOutput
         self.comment = comment
 
     def clone(self):
         elem = Element()
         elem.inputs = self.inputs
-        elem.expectedOutputs = self.expectedOutputs
+        elem.expectedOutput = self.expectedOutput
         return elem
+
+    def show(self):
+        print(self.inputs)
+        print(self.expectedOutput)
 
 
 class TeachingSet:
@@ -46,7 +68,11 @@ class TeachingSet:
 
     def normalize(self):
         for item in self.listOfElements:
-            Neuron.normalize(Neuron(), item.inputs)
+            normalize(item.inputs)
+
+    def show(self):
+        for element in self.listOfElements:
+            element.show()
 
     def readFromConsole(self):
         print("Enter number of inputs:")
@@ -55,17 +81,65 @@ class TeachingSet:
         self.outputCount = int(input())
         pass
 
+    def count(self):
+        return len(self.listOfElements)
+
+
+class ProgramLogic:
+    currentElementIndex = 0
+    currentNormalizedInputs = []
+    previousWeights = []
+    previousResponse = [0.]
+    previousError = [0.]
+    currentResponse = 0.
+    currentError = 0.
+    examinedNeuron = Neuron
+    teachingSet = TeachingSet
+    currentElement = Element
+    history = []
+
+    def __init__(self, teachingSet, neuron):
+        self.teachingSet = teachingSet
+        self.examinedNeuron = neuron
+
+    def performTeaching(self, teachingRatio):
+
+        if self.currentElementIndex >= self.teachingSet.count():
+            self.currentElementIndex = 0
+
+        self.currentElement = self.teachingSet.listOfElements[self.currentElementIndex]
+        self.currentNormalizedInputs = list(self.currentElement.inputs)
+        normalize(self.currentNormalizedInputs)
+
+        self.examinedNeuron.learn(
+            self.currentNormalizedInputs,
+            self.currentElement.expectedOutput,
+            teachingRatio,
+            self.previousResponse,
+            self.previousError
+            )
+
+        self.currentResponse = self.examinedNeuron.response(self.currentNormalizedInputs)
+        self.currentError = self.currentElement.expectedOutput - self.currentResponse
+        self.history.append(float(abs(self.currentError)))
+        self.currentElementIndex += 1
+
 
 print("1 neuron with 5 inputs and 1 output")
 teachingSet = TeachingSet(5, 1)
-teachingSet.listOfElements.append(Element([3, 4, 3, 4, 5], [1], "Typical object to accept"))
-teachingSet.listOfElements.append(Element([1, -2, 1, -2, 4], [-1], "Typical object to reject"))
-teachingSet.listOfElements.append(Element([4, 2, 5, 3, 2], [0.8], "Untypical object to accept"))
-teachingSet.listOfElements.append(Element([3, 4, 3, 4, 5], [-0.8], "Untypical object to reject"))
+teachingSet.listOfElements.append(Element([3, 4, 3, 4, 5], 1, "Typical object to accept"))
+teachingSet.listOfElements.append(Element([1, -2, 1, -2, 4], -1, "Typical object to reject"))
+teachingSet.listOfElements.append(Element([4, 2, 5, 3, 2], 0.8, "Untypical object to accept"))
+teachingSet.listOfElements.append(Element([0, -1, 0, -3, -3], -0.8, "Untypical object to reject"))
 
+neuron = Neuron()
+neuron.randomize(-0.1, 0.1, 5)
+pl = ProgramLogic(teachingSet, neuron)
 
+print("How many times do you want to train the network?")
+amount = int(input())
+for i in range(amount):
+    pl.performTeaching(0.5)
 
-
-
-
+print("\n", pl.history)
 
