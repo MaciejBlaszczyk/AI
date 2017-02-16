10984559f81a6edb47936327db10bd1f418a1262
Maciej Blaszczyk <maciej_blaszczyk96@wp.pl>
2017-02-16 12:13:52 +0100

    file reorganized a bit

--- Network02.py
+++ Network02.py
@@ -1,28 +1,28 @@
 class Neuron:
-    weights = []
-    prevWeights = []
+
+    def __init__(self, initialWeights):
+        self.weights = initialWeights
 
 
 class LinearNetwork:
-    neurons = []
-
+    """network which contains neurons"""
     def __init__(self, initialWeights):
-        numberOfNeurons = len(initialWeights)
-        for i in range(numberOfNeurons - 1):
-            self.neurons.append(Neuron())
-            self.neurons[i].weights = initialWeights[i]
+        self.numberOfNeurons = len(initialWeights)
+        self.neurons = []
+        for i in range(self.numberOfNeurons):
+            self.neurons.append(Neuron(initialWeights[i]))
 
 
 classNames = ["mammal", "bird", "fish"]
 featureNames = ["number of legs ", "lives in water", "can fly       ", "has feathers  ", "egg-laying    "]
-
 weights = [
     [ 4.00,  0.01, 0.01, -1.00, -1.50],
     [ 2.00, -1.00, 2.00,  2.50,  2.00],
-    [-1.00,  3.50, 0.01, -2.00,  1.50]
-]
+    [-1.00,  3.50, 0.01, -2.00,  1.50]]
+features = []
+questions = ["How many legs does it have?", "Does it live in water?", "Can it fly?",
+             "Does it have feathers?", "Does it lay eggs?"]
 
-network = LinearNetwork(weights)
 
 print("                     " + "       ".join(classNames))
 for i in range(5):
@@ -30,13 +30,13 @@ for i in range(5):
     print(featureNames[i], end = "")
     for j in range(3):
         print( "        " + str(weights[j][i]), end = "")
-
-features = []
-questions = ["How many legs does it have?", "Does it live in water?", "Can it fly?",
-             "Does it have feathers?", "Does it lay eggs?"]
 print("\nEnter features of your animal:")
-print("If you want to answet the question \"yes\", write 1, if \"no\", write -1, "
+print("If you want to answer the question \"yes\", write 1, if \"no\", write -1, "
       "if you are not sure, write a number between -1 and 1 ")
+
+
+network = LinearNetwork(weights)
+
 for i in range(5):
     print(questions[i])
     features.append(float(input()))
@@ -44,10 +44,11 @@ for i in range(5):
 mammal = 0.
 bird = 0.
 fish = 0.
+
 for i in range(5):
-    mammal += weights[0][i] * features[i]
-    bird += weights[1][i] * features[i]
-    fish += weights[2][i] * features[i]
+    mammal += network.neurons[0].weights[i] * features[i]
+    bird += network.neurons[1].weights[i] * features[i]
+    fish += network.neurons[2].weights[i] * features[i]
 
 if mammal > bird and mammal > fish and mammal > 2:
     print("It is a mammal")
